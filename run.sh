dea00caa9c86f85ba0314808972cc4cbae02e80f
Mario Palka <mario.palka@gmail.com>
2021-02-24 16:38:51 +0100

    fix DISPLAY in run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,18 @@
 # directory where this script is located
 cd "$(dirname "$0")" || exit
 
-xhost + 127.0.0.1
+if grep -qi Linux /proc/version; then
+    export DISPLAY=:0
+else
+    export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+fi
+
+xhost +
 
 docker run \
-    -it --rm -e DISPLAY=host.docker.internal:0 \
+    -it --rm \
+    -e DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd)/workspace:/home/developer/modelio/workspace:z \
     -v $(pwd)/.modelio:/home/developer/.modelio:z \
     --net=host --ipc=host \
