41b5905d626c71a80feb0f4d87d51ad7f19458ec
Mariyo <mario.palka@gmail.com>
2021-02-24 14:50:37 +0100

    Add .modelio volume

--- run.sh
+++ run.sh
@@ -5,4 +5,9 @@ cd "$(dirname "$0")" || exit
 
 xhost + 127.0.0.1
 
-docker run -it --rm -e DISPLAY=host.docker.internal:0 -v $(pwd)/workspace:/home/developer/modelio/workspace:z --net=host --ipc=host modelio
+docker run \
+    -it --rm -e DISPLAY=host.docker.internal:0 \
+    -v $(pwd)/workspace:/home/developer/modelio/workspace:z \
+    -v $(pwd)/.modelio:/home/developer/.modelio:z \
+    --net=host --ipc=host \
+    mariyo/modelio:4.1.0
